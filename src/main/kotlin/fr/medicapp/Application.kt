e15c2e97270e7f451004c141049c27e210a6ba18
Lana Heyrendt <LanaHeyrendt@la-taniere-solidaire.gay>
2024-01-03 14:58:20 +0100

    Initial commit

---
+++ src/main/kotlin/fr/medicapp/Application.kt
@@ -0,0 +1,18 @@
+package fr.medicapp
+
+import fr.medicapp.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
+}
---
+++ src/main/kotlin/fr/medicapp/plugins/Security.kt
@@ -0,0 +1,43 @@
+package fr.medicapp.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.ktor.util.*
+
+fun Application.configureSecurity() {
+    authentication {
+        val myRealm = "MyRealm"
+        val usersInMyRealmToHA1: Map<String, ByteArray> = mapOf(
+            // pass="test", HA1=MD5("test:MyRealm:pass")="fb12475e62dedc5c2744d98eb73b8877"
+            "test" to hex("fb12475e62dedc5c2744d98eb73b8877")
+        )
+
+        digest("myDigestAuth") {
+            digestProvider { userName, realm ->
+                usersInMyRealmToHA1[userName]
+            }
+        }
+    }
+    data class MySession(val count: Int = 0)
+    install(Sessions) {
+        cookie<MySession>("MY_SESSION") {
+            cookie.extensions["SameSite"] = "lax"
+        }
+    }
+    routing {
+        authenticate("myDigestAuth") {
+            get("/protected/route/digest") {
+                val principal = call.principal<UserIdPrincipal>()!!
+                call.respondText("Hello ${principal.name}")
+            }
+        }
+        get("/session/increment") {
+            val session = call.sessions.get<MySession>() ?: MySession()
+            call.sessions.set(session.copy(count = session.count + 1))
+            call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+    }
+}
