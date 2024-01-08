120e5d7d118d1f0e798209d7a612cfd27d6fd0d4
Lana Heyrendt <LanaHeyrendt@la-taniere-solidaire.gay>
2024-01-08 21:28:58 +0100

    Create most of the non security related routing

--- src/main/kotlin/fr/medicapp/plugins/Routing.kt
+++ src/main/kotlin/fr/medicapp/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package fr.medicapp.plugins
 
+import fr.medicapp.dao.DaoMedication
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,6 +10,47 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        /*
+        // Find User from email and send it back if pass
+        get("/user") {
+
+        }
+
+        post("/user") {
+
+        }
+
+        delete("/user") {
+
+        }
+         */
+
+        get("medication/byCisCode/{ciscode}") {
+            if (call.parameters["ciscode"] != null) {
+                val med = DaoMedication.INSTANCE.getFromCisCode(call.parameters["ciscode"]!!)
+                if (med != null) {
+                    call.respond(med)
+                } else { // if not medication is found, the CisCode is supposed invalid
+                    call.respondText("Invalid CisCode", status = HttpStatusCode.NotFound)
+                }
+            } else {
+                call.respondText("Missing parameters", status = HttpStatusCode.BadRequest)
+            }
+        }
+
+        get("medication/byName/{name}") {
+            if (call.parameters["ciscode"] != null) {
+                val med = DaoMedication.INSTANCE.getFromName(call.parameters["name"]!!)
+                if (med.isNotEmpty()) {
+                    call.respond(med)
+                } else {
+                    call.respondText("Medication not found", status = HttpStatusCode.NotFound)
+                }
+            } else {
+                call.respondText("Missing parameters", status = HttpStatusCode.BadRequest)
+            }
         }
     }
 }
