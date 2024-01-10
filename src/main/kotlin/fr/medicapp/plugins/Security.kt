651bdb8b63e4055fe6bb6ee9519509b58cf4dfd1
E211439A <L4v3r1T3>
2024-01-10 16:16:37 +0100

    WTH am i doing ?

--- src/main/kotlin/fr/medicapp/plugins/Security.kt
+++ src/main/kotlin/fr/medicapp/plugins/Security.kt
@@ -1,5 +1,6 @@
 package fr.medicapp.plugins
 
+import fr.medicapp.dao.DaoUser
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
@@ -7,37 +8,42 @@ import io.ktor.server.routing.*
 
 fun Application.configureSecurity() {
     authentication {
-        basic(name = "myauth1") {
-            realm = "Ktor Server"
+        basic(name = "authenticationAPI") {
+            realm = "User login"
             validate { credentials ->
-                if (credentials.name == credentials.password) {
-                    UserIdPrincipal(credentials.name)
-                } else {
+                val tryUser = DaoUser.INSTANCE.getByEmail(credentials.name)
+                if (tryUser ==  null){
                     null
+                }else {
+                    if (tryUser.passwordHash == credentials.password) {
+                        UserIdPrincipal(tryUser.id)
+                    } else {
+                        null
+                    }
                 }
             }
         }
 
-        form(name = "myauth2") {
+        /*form(name = "authenticationAPIWithUser") {
             userParamName = "user"
             passwordParamName = "password"
             challenge {
-                /**/
+
             }
-        }
+        }*/
     }
     routing {
-        authenticate("myauth1") {
+        authenticate("authenticationAPI") {
             get("/protected/route/basic") {
                 val principal = call.principal<UserIdPrincipal>()!!
                 call.respondText("Hello ${principal.name}")
             }
         }
-        authenticate("myauth2") {
+        /*authenticate("authenticationAPIWithUser") {
             get("/protected/route/form") {
                 val principal = call.principal<UserIdPrincipal>()!!
                 call.respondText("Hello ${principal.name}")
             }
-        }
+        }*/
     }
 }
